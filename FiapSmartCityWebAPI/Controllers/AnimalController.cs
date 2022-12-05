112be1e7d9427723a094979d04eaafd718ff1526
Marianna Magno Nascimento <mari.magno.osorio@avanade.com>
2022-12-05 19:10:43 -0300

    Add project files.

---
+++ FiapSmartCityWebAPI/Controllers/AnimalController.cs
@@ -0,0 +1,85 @@
+﻿using FiapSmartCityWebAPI.Repository;
+using FiapSmartCityWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapSmartCityWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        public readonly AnimalRepository animalRepository;
+
+        public AnimalController()
+        {
+            animalRepository = new AnimalRepository();
+        }
+
+        [HttpGet]
+        [Route("GetAnimal")]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(new AnimalRepository().Listar());
+            }
+            catch (Exception)
+            {
+                return NotFound();
+                throw;
+            }
+        }
+        [HttpPost(Name = "GetAnimal")]
+        
+        public IActionResult Post([FromBody] Animal animal)
+        {
+            try
+            {
+                new AnimalRepository().Create(animal);
+                string location = "http://localhost:7013/FiapSmartCityWebAPI";
+
+                return Created(new Uri(location), animal);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                throw new Exception("Erro ao cadastrar os dados no banco");
+            }
+        }
+
+        [HttpDelete(Name = "GetAnimal")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                animalRepository.Delete(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                throw new Exception("Erro ao cadastrar os dados no banco");
+
+            }
+        }
+
+            [HttpPut(Name = "GetAnimal")]
+            public IActionResult Edit([FromBody] Animal animal)
+            {
+                try
+                {
+                    animalRepository.Edit(animal);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                    throw new Exception("Erro ao cadastrar os dados no banco");
+
+                }
+            }
+        }
+    }
+
+    
---
+++ FiapSmartCityWebAPI/Repository/AnimalRepository.cs
@@ -0,0 +1,39 @@
+﻿using FiapSmartCityWebAPI.Models;
+using FiapSmartCityWebAPI.Repository.Context;
+
+
+
+namespace FiapSmartCityWebAPI.Repository
+{
+    public class AnimalRepository
+    {
+        public readonly DataBaseContext context;
+        public AnimalRepository()
+        {
+            context = new DataBaseContext();
+        }
+        public IList<Animal> Listar()
+        {
+            return context.Animal.ToList();
+        }
+        public void Create(Animal animal)
+        {
+            context.Animal.Add(animal);
+            context.SaveChanges();
+        }
+        public void Edit(Animal animal)
+        {
+            context.Animal.Update(animal);
+            context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var animal = new Animal()
+            {
+                IdAnimal = id
+            };
+            context.Animal.Remove(animal);
+            context.SaveChanges();
+        }
+    }
+}
